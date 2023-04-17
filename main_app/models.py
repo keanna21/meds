c55de00ebbfeca6e2cff91e4d4a2c7948aa6c476
Maria Grassa <mgrassa00gmail.com>
2023-04-16 17:49:55 -0700

    added med model, admin, & detail page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Med(models.Model):
+  name = models.CharField(max_length=100)
+  quantity = models.IntegerField()
+  perscribed = models.DateField()
+  expiration = models.DateField()
+  instructions = models.TextField(max_length=250)
+  
+  def __str__(self):
+    return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home page
     path('', views.home, name='home'),
+    # index page that shows all medications within database
     path('meds/', views.meds_index, name='index'),
+    # details page for each mediction
+    path('meds/<int:med_id>/', views.meds_detail, name='detail'),
 ]
