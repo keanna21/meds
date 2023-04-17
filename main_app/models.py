1f36f4cc84453c459d805f64a06d1b746367417c
Maria Grassa <mgrassa00gmail.com>
2023-04-17 12:08:30 -0700

    added time picker & fixed when_taken form

--- main_app/models.py
+++ main_app/models.py
@@ -16,11 +16,16 @@ class Med(models.Model):
     return reverse('detail', kwargs={'med_id': self.id})
   
 class WhenTaken(models.Model):
-  date = models.DateTimeField()
+  date = models.DateField()
+  time = models.CharField(max_length=100)
   quantity = models.IntegerField()
   
   # Foreign Key
   med = models.ForeignKey(Med, on_delete=models.CASCADE)
   
   def __str__(self):
-    return f"{self.date} {self.quantity}"
+    # make time & date more readable
+    return f"{self.date}"
+  
+  class Meta:
+    ordering = ['-date', '-time']
