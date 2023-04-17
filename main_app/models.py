c2a7b952df4b1ff16fc8381f9e8d3b5b0aeb5c9c
tristanH77 <tristanhartman77@gmail.com>
2023-04-17 16:04:30 -0400

    add login and auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Med(models.Model):
@@ -8,6 +9,7 @@ class Med(models.Model):
   perscribed = models.DateField()
   expiration = models.DateField()
   instructions = models.TextField(max_length=250)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
     return self.name
