0e890805b0604a9e0d51389747a54b0d2269fc3a
keanna21 <keannathompson@yahoo.com>
2023-04-17 09:58:08 -0500

    add full CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('meds/', views.meds_index, name='index'),
     # details page for each mediction
     path('meds/<int:med_id>/', views.meds_detail, name='detail'),
+    path('meds/create/', views.MedCreate.as_view(), name='meds_create'),
+    path('meds/<int:pk>/update/', views.MedUpdate.as_view(), name='meds_update'),
+    path('meds/<int:pk>/delete/', views.MedDelete.as_view(), name='meds_delete'),
 ]
