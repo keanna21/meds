642b74c9028b626ad1d982ebade1d801c3e8cbb0
tristanH77 <tristanhartman77@gmail.com>
2023-04-20 15:56:33 -0400

    first deployment

--- meds/settings.py
+++ meds/settings.py
@@ -9,24 +9,41 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import dj_database_url
+import environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# this sets all our environment variables to there required
+# values in production if an enivronment variable 
+# is absent, so False, True, True respectively
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    CSRF_COOKIE_SECURE=(bool, True),
+    SESSION_COOKIE_SECURE=(bool, True),   
+)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lr7(nxeu!i=x964ipa3nxd1vdpekx1o-go=yme2=zs57le%+!u'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY'] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
+CSRF_COOKIE_SECURE = env('CSRF_COOKIE_SECURE')
+SESSION_COOKIE_SECURE = env('SESSION_COOKIE_SECURE')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['falling-sea-1616.fly.dev', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['https://falling-sea-1616.fly.dev']
 
 # Application definition
 
@@ -75,10 +92,9 @@ WSGI_APPLICATION = 'meds.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'meds',
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
@@ -117,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "static"
 # Add this variable to specify where successful logins should redirect to
 LOGIN_REDIRECT_URL = '/meds/'
 LOGOUT_REDIRECT_URL = '/'
